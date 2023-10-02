953ff30b9f094b856c47086d0c555c6d350a64f6
Горячев_Михаил <klasdmwer@gmail.com>
2023-10-02 21:28:05 +0300

    Добавьте файлы проекта.

---
+++ dz69/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz69
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
